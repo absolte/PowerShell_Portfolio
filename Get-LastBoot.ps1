381dbb9851e4cd8866df66692b65f8147dd0fb0a
absolte <51381463+absolte@users.noreply.github.com>
2020-01-13 19:49:08 -0500

    Add functions to $Profile

---
+++ Get-LastBoot.ps1
@@ -0,0 +1,12 @@
+﻿Function Get-LastBoot
+{
+  param (
+    [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+		[String[]]$ComputerName = $env:COMPUTERNAME
+  ) 
+
+  Get-WmiObject -Class Win32_OperatingSystem -ComputerName $ComputerName | Select-Object -Property CSName,@{n="Last Booted";e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
+  
+}
+
+Get-LastBoot
